4b8834b8d092bf3287bc9b45fd2df4c8c7b0f788
venupriya1 <priyanikavenu@gmail.com>
2023-10-23 20:14:28 -0700

    persons class

--- week4/classes_homework.js
+++ week4/classes_homework.js
@@ -1,9 +1,9 @@
 //hamster
 class Hamster{
-    constructor(owner,name,price){
-        this.owner=owner;
-        this.name=name;
-        this.price=price;
+    constructor(name){
+        this.owner=''
+        this.name=name
+        this.price=15
     }
     wheelRun(){
         console.log('squeak squeak');
@@ -12,18 +12,19 @@ class Hamster{
         console.log("nibble nibble");
     }
     getPrice(){
-        console.log('return the price');
+        return this.price
     }
 }
+
 class person{
-    constructor(name,age,height,weight,mood,hamsters,bankAccount){
-        this.name=sam;
+    constructor(name){
+        this.name=name;
         this.age=0;
-        height=0;
-        weight=0;
-        mood=0;
-        hamsters=[];
-        bankAccount=0;
+        this.height=0;
+        this.weight=0;
+        this.mood=0;
+        this.hamsters=[];
+        this.bankAccount=0;
     }
     getName(){
         return this.name
@@ -35,7 +36,7 @@ class person{
         return this.weight
     }
     greet(){
-        console.log("hi this is sam");
+        console.log("hi this is ", this.name);
     }
     eat(){
         this.weight++
@@ -51,10 +52,10 @@ class person{
     this.mood--
     this.bankAccount+=10
    } 
-   buyHamster(){
-    this.hamsters.push('hamster')
+   buyHamster(hamster){
+    this.hamsters.push(hamster)
     this.mood+=10
-    bankAccount-='hamster'.getPrice()
+    this.bankAccount-=hamster.getPrice()
    }
     
 }
